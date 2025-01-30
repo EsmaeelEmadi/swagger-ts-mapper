bc8993ba43929ea7a46e3e23cbda0335686f4494
Emad <93305987+EsmaeelEmadi@users.noreply.github.com>
2025-01-30 17:47:26 +0330

    Feat/add swagger schema to ts #4 (#5)
    * feat(setup): add files, exports and build script
    
    * chore(config): revise tsconfig for build
    
    * feat(core): add body

---
+++ src/body.ts
@@ -0,0 +1,88 @@
+type TProperties = {
+    [key: string]: TSchema;
+};
+
+type TSchema = {
+    type?: unknown;
+    nullable?: boolean;
+    enum?: unknown;
+    items?: TProperties | { type: unknown };
+    required?: readonly string[];
+    properties?: TProperties;
+    oneOf?: readonly TSchema[];
+    allOf?: readonly TSchema[];
+    anyOf?: readonly TSchema[];
+};
+
+type UnionToIntersection<U> = (
+    U extends unknown
+        ? (k: U) => void
+        : "never-1"
+) extends (k: infer I) => void
+    ? I
+    : "never-2";
+
+export type TBodyMapper<T extends TSchema | readonly TSchema[]> =
+    T extends TSchema
+        ? T["type"] extends "object"
+            ? T["properties"] extends TProperties
+                ? {
+                      // Required properties
+                      [K in keyof T["properties"] as T extends {
+                          required: readonly string[];
+                      }
+                          ? K extends T["required"][number]
+                              ? K
+                              : never
+                          : never]-?: TBodyMapper<T["properties"][K]>;
+                  } & {
+                      // Optional properties
+                      [K in keyof T["properties"] as T extends {
+                          required: readonly string[];
+                      }
+                          ? K extends T["required"][number]
+                              ? never
+                              : K
+                          : K]?: TBodyMapper<T["properties"][K]>;
+                  }
+                : Record<string, unknown>
+            : T["type"] extends "string"
+              ? "enum" extends keyof T
+                  ? T["enum"] extends readonly string[]
+                      ? T["enum"][number]
+                      : never
+                  : string
+              : T["type"] extends "number" | "integer"
+                ? "enum" extends keyof T
+                    ? T["enum"] extends readonly number[]
+                        ? T["enum"][number]
+                        : never
+                    : number
+                : T["type"] extends "boolean"
+                  ? boolean
+                  : T["type"] extends "array"
+                    ? "items" extends keyof T
+                        ? T["items"] extends { type: unknown }
+                            ? TBodyMapper<T["items"]>[]
+                            : never
+                        : never
+                    : T["type"] extends "null"
+                      ? null
+                      : "oneOf" extends keyof T
+                        ? T["oneOf"] extends readonly TSchema[]
+                            ? TBodyMapper<T["oneOf"]>
+                            : never
+                        : "allOf" extends keyof T
+                          ? T["allOf"] extends readonly TSchema[]
+                              ? UnionToIntersection<
+                                    TBodyMapper<T["allOf"][number]>
+                                >
+                              : never
+                          : "anyOf" extends keyof T
+                            ? T["anyOf"] extends readonly TSchema[]
+                                ? TBodyMapper<T["anyOf"]>
+                                : never
+                            : unknown
+        : T extends readonly TSchema[]
+          ? TBodyMapper<T[number]>
+          : unknown;
